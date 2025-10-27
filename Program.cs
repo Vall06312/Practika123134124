3cf894a7a2723826d0e6584fcb76099ea603e68b
Валера <kolesnikovvalerij275@gmail.com>
2025-10-27 18:46:55 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Person
+{
+    public string name = "Валера"; 
+    public int age = 18;
+
+    public void Print()
+    {
+        Console.WriteLine($"Имя: {name} Возраст: {age}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Print();
+
+        Console.ReadLine();
+    }
+}
